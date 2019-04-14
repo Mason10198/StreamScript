59fe6d89d72c19161fa80097bf96ac896b8877d0
Mason10198 <31994327+Mason10198@users.noreply.github.com>
2019-04-14 17:43:37 -0500

    Update StreamScript.sh

--- StreamScript.sh
+++ StreamScript.sh
@@ -2,7 +2,7 @@
 
 ### Info / Config ###
 title=StreamScript # Title to display
-version=v0.1 # Version to display
+version=v0.2 # Version to display
 stream=rtmp://192.168.1.4/live/tv # Stream URL to connect
 delay=30 # Delay (secs) between connection attempts
 startdelay=10s # Startup delay (specify unit)
@@ -10,7 +10,8 @@ log=/home/pi/streamscript.log # Location & name of logfile
 
 echo $title" "$version" starting in 10 seconds..."
 date2=`date +"%R %A, %B %d"`
-NUM=1
+succ=0
+fail=0
 sleep $startdelay
 
 while :
@@ -21,7 +22,9 @@ tput civis
 date=`date`
 echo $title" "$version" | "$date2
 echo "Stream: "$stream
-echo $NUM" connection attempts since boot."
+echo "-Connection attempts since boot-"
+echo "Successful: "
+echo "Failed: "
 printf '%*s\n' "${COLUMNS:-$(tput cols)}" '' | tr ' ' - # Draw horizontal line
 echo ""
 
@@ -31,7 +34,7 @@ omxplayer -o hdmi --live $stream > /dev/null & # OMXPlayer stream command
 
 # Spinning progress indicator
 pid=$! # Process Id of OMXPlayer
-
+time=0
 spin='-\|/'
 
 i=0
@@ -40,11 +43,13 @@ do
   i=$(( (i+1) %4 ))
   printf "\rSearching for stream... ${spin:$i:1}"
   sleep .1
+  let "time++"
 done
 
+time=(($time / 10))
 wait_time=$delay
 
-printf "\rStream disconnected or not found."
+printf "\rStream disconnected or not found. Time: "$time
 echo ""
 temp_cnt=${wait_time}
 dots=""
